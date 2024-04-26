7c2c739eb1451da00211c6e9f35c4d46d80cf276
Robin Kolasinski <robin.kolasinski@gmail.com>
2024-04-26 11:57:37 +0200

    added resetDB file and hashing user password

--- backend/src/entities/User.ts
+++ backend/src/entities/User.ts
@@ -1,6 +1,6 @@
 import { BaseEntity, Column, Entity, PrimaryGeneratedColumn} from "typeorm";
 import { Field, ID, ObjectType } from "type-graphql";
-
+import { argon2id, hash, verify } from "argon2";
 
 @Entity()
 @ObjectType()
@@ -24,3 +24,17 @@ export default class User extends BaseEntity{
     @Column()
     hashedPassword: string;
 }
+const hashingOptions = {
+    memoryCost: 2 ** 16,
+    timeCost: 5,
+    type: argon2id,
+  };
+  
+  export const hashPassword = async (plainPassword: string): Promise<string> =>
+    await hash(plainPassword, hashingOptions);
+  
+  export const verifyPassword = async (
+    plainPassword: string,
+    hashedPassword: string
+  ): Promise<boolean> =>
+    await verify(hashedPassword, plainPassword, {secret: hashingOptions});
---
+++ backend/src/resetdb.ts
@@ -0,0 +1,36 @@
+import db from "./db";
+import User, { hashPassword } from "./entities/User";
+
+export async function clearDB() {
+    const runner = db.createQueryRunner();
+    await runner.query("SET session_replication_role = 'replica'");
+    await Promise.all(
+      db.entityMetadatas.map(async (entity) =>
+        runner.query(`ALTER TABLE "${entity.tableName}" DISABLE TRIGGER ALL`)
+      )
+    );
+    await Promise.all(
+      db.entityMetadatas.map(async (entity) =>
+        runner.query(`DROP TABLE IF EXISTS "${entity.tableName}" CASCADE`)
+      )
+    );
+    await runner.query("SET session_replication_role = 'origin'");
+    await db.synchronize();
+  }
+  
+  export default async function main() {
+    await db.initialize();
+    await clearDB();
+  
+    const admin = User.create({
+        firstName: "John",
+        lastName: "Doe",
+        email: "admin@app.com",
+        hashedPassword: await hashPassword("adminadmin"),
+    });
+  
+    await db.destroy();
+    console.log("done !");
+  }
+  
+  main();
