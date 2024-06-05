119c741ba9e1b166f7207474abc804496f4a4b56
Alexandre <richert.alexandre31@gmail.com>
2024-06-05 09:18:39 +0200

    mise en place de variables globales de style

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,12 +2,22 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
 import client from "@/graphql/client";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const colors = {
+  brand: {
+    100: "#003b1e",
+    900: "#003b1e",
+  },
+};
+
+const theme = extendTheme( { colors });
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
       <ApolloProvider client={client}>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
         <Component {...pageProps} />
     </ChakraProvider>
       </ApolloProvider>
