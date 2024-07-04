1b3e511e2d2f96689ffe9167e917bd9f5d807752
Robin <robin.kolasinski@gmail.com>
2024-07-04 11:41:52 +0200

    login page style

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,6 +1,6 @@
 import { useLoginMutation } from "@/graphql/generated/schema";
-import { Button, Card, Center, FormControl, FormLabel, Input, InputGroup, InputRightElement } from "@chakra-ui/react";
-
+import { Box, Button, Card, Center, Flex, FormControl, FormLabel, Heading, IconButton, Image, Input, InputGroup, InputRightElement, Link, Text } from "@chakra-ui/react";
+import { ArrowLeft } from "lucide-react";
 import { FormEvent, useState } from "react";
 
 const Login = () => {
@@ -24,33 +24,55 @@ const Login = () => {
 
   return (
     <>
-      <Center>
-        <Card>
+    <Link href='/'>
+        <IconButton
+          aria-label="Back"
+					bg="transparent"
+					boxShadow="none"
+          _hover={{ bg: "gray.200" }}
+          icon={<ArrowLeft color="#22543D"/>}
+        />
+        </Link>
+      <Heading as="h1" fontSize='6xl' fontWeight="bold" mt={8} textAlign="center" color="green.800">Welcome Back</Heading>
+      <Image src="/Gifty-logo.svg" alt="Gifty" mt={8} w="20%" mx="40%"/>
+      <Center>    
+      <Box
+        mx="24px"
+        mt="8px"
+        p={4}
+        maxW="500px"
+        w="90%"
+        data-testid="card"
+        bgColor="transparent"
+        border="none"
+        boxShadow="none"
+      >
           <form onSubmit={handleSubmit}>
             <FormControl>
-              <FormLabel>Adresse email</FormLabel>
-              <Input isRequired placeholder="Enter your email" autoComplete="" type="email" id="email" name="email"/>
+              <FormLabel fontSize={14} fontWeight="bold" mb={1} color="green.800">Adresse mail</FormLabel>
+              <Input isRequired autoComplete="" type="email" id="email" name="email" borderRadius={15} borderColor="green.800" />
 
-              <FormLabel>Mot de passe</FormLabel>
-               <InputGroup size='md'>
+              <FormLabel mt={4} fontSize={14} fontWeight="bold" mb={1} color="green.800" >Mot de passe</FormLabel>
                 <Input
                   name="password" id="password" isRequired
                   type={show ? 'text' : 'password'}
-                  placeholder='Enter password'
+                  borderRadius={15} borderColor="green.800"
                 />
-                <InputRightElement width='4.5rem'>
-                  <Button h='1.75rem' size='sm' onClick={handleClick}>
-                    {show ? 'Hide' : 'Show'}
-                  </Button>
-                </InputRightElement>
-              </InputGroup>
-            <Button type="submit">
-              Se connecter
+                <Flex justify="flex-end" mt={2}>
+                <Link color="gray.400" fontSize={12}  _hover={{ bg: "gray.200" }} p={1} borderRadius="md">Mot de passe oublié ?</Link>
+                </Flex>
+              <Center>
+            <Button type="submit" my={4} variant="goldenButton" py={5} px={6}>
+              <Text fontSize={18}>Se Connecter</Text>
             </Button>
+            </Center>
             </FormControl>
           </form>
-        </Card>
+        </Box>
       </Center>
+      <Box ml={6} mt={16}>
+              <Text>Pas encore inscrit ? <Link href="/signup" color="gray.400">Inscrivez-vous</Link></Text>
+            </Box>
     </>
 );
 }
