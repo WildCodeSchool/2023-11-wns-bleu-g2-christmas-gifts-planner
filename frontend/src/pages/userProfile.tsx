b81c8308caecbc3993ef761030bab1887ae98eed
Robin <robin.kolasinski@gmail.com>
2024-07-10 12:14:19 +0200

    refactoring

--- frontend/src/pages/userProfile.tsx
+++ frontend/src/pages/userProfile.tsx
@@ -68,102 +68,49 @@ const UserProfile = () => {
     return(
         <>
         <Link href='/'>
-        <IconButton
-          aria-label="Back"
-					bg="transparent"
-					boxShadow="none"
-          _hover={{ bg: "gray.200" }}
-          icon={<ArrowLeft color="#22543D"/>}
-        />
+            <IconButton aria-label="Back" bg="transparent" boxShadow="none" _hover={{ bg: "gray.200" }} icon={<ArrowLeft color="#22543D"/>}/>
         </Link>
         <Center>
-        <Box mx="24px" mt="8px" p={4} maxW="500px" w="90%" data-testid="card" bgColor="transparent" border="none" boxShadow="none">
+            <Box mx="24px" mt="8px" p={4} maxW="500px" w="90%" data-testid="card" bgColor="transparent" border="none" boxShadow="none">
                 <form onSubmit={handleSubmit}>
-  <FormControl bgColor="#FFFEF9">
-      {/* Firstname and lastname */}
-      <Grid templateColumns="repeat(2, 1fr)" gap={4} mt={4}>
-        <GridItem>
-          <Input
-            type="text"
-            name="firstName"
-            id="firstName"
-            minLength={2}
-            maxLength={30}
-            isRequired
-            placeholder="Nom"
-            width="100%"
-            borderRadius={15}
-						borderColor="green.600"
-          />
-        </GridItem>
-        <GridItem>
-          <Input
-            type="text"
-            name="lastName"
-            id="lastName"
-            minLength={2}
-            maxLength={30}
-            isRequired
-            placeholder="Prénom"
-            width="100%"
-            borderRadius={15}
-						borderColor="green.600"
-          />
-        </GridItem>
-      </Grid>
-      {/* Email */}
-      {error === 2 ? 
-                <Text position="absolute" fontSize={14} fontWeight="bold" color="red.700">Cet e-mail existe déjà</Text>
-              : ""}
-        <Input type='email' 
-        isRequired
-        id="email" 
-        data-testid="label-email" 
-        name="email" 
-        placeholder="Adresse mail" 
-        my={6}
-        borderRadius={15}
-				borderColor={error === 2 ? "red.700" : "green.600"}
-        />
-{/* Password and confirm Password */}
-        <InputGroup size='md'>
-              {error === 1 &&
-                <Text position="absolute" mt={-6} fontSize={14} fontWeight="bold" color="red.700">Les mots de passe ne correspondent pas !</Text>
-               }
-               {error === 4 && <Text position="absolute" mt={-6} fontSize={14} fontWeight="bold" color="red.700" >
-                Ce mot de passe n&apos;est pas autorisé 
-                <Tooltip label={arrayOfErrors.map((error, index) => (<Text key={index} fontSize={12}>{error}</Text>))
-                }>
-                  <IconButton icon={<InfoIcon height={22}/>} aria-label="seeMore" h={4} variant="none" bgColor="transparent" boxShadow="none" /></Tooltip></Text> }
-
-
-          <Input
-          name="password" 
-          id="password" 
-          isRequired
-          type='password'
-          placeholder='Mot de passe'
-          borderRadius={15}
-					borderColor={error === 1 || error === 4 ? "red.700" : "green.600"}
-          />
-        </InputGroup>
-        <InputGroup size='md' mt={6} zIndex={0}>
-          <Input
-          name="passwordConfirmation" zIndex={0}
-          id="passwordConfirmation" 
-          isRequired
-          type={'password'}
-          placeholder='Confirmer le mot de passe'
-          borderRadius={15}
-					borderColor={error === 1 || error === 4 ? "red.700" : "green.600"}
-          />
-        </InputGroup>
-		    <Center>
-          <Button variant="goldenButton" type="submit" mt={8}>
-            Modifier
-          </Button>
-		    </Center>
-      </FormControl>
+                    <FormControl bgColor="#FFFEF9">
+                        {/* Firstname and lastname */}
+                        <Grid templateColumns="repeat(2, 1fr)" gap={4} mt={4}>
+                            <GridItem>
+                                <Input type="text" name="firstName" id="firstName" minLength={2} maxLength={30} isRequired placeholder="Nom" width="100%" borderRadius={15} borderColor="green.600"/>
+                            </GridItem>
+                            <GridItem>
+                                <Input type="text" name="lastName" id="lastName" minLength={2} maxLength={30} isRequired placeholder="Prénom" width="100%" borderRadius={15} borderColor="green.600"/>
+                            </GridItem>
+                        </Grid>
+                        {/* Email */}
+                        {error === 2 &&
+                                <Text position="absolute" fontSize={14} fontWeight="bold" color="red.700">Cet e-mail existe déjà</Text>
+                                }
+                        <Input type='email' isRequired id="email" data-testid="label-email" name="email" placeholder="Adresse mail" my={6} borderRadius={15} borderColor={error === 2 ? "red.700" : "green.600"}/>
+                        {/* Password and confirm Password */}
+                        <InputGroup size='md'>
+                            {error === 1 &&
+                                <Text position="absolute" mt={-6} fontSize={14} fontWeight="bold" color="red.700">Les mots de passe ne correspondent pas !</Text>
+                                }
+                            {error === 4 && <Text position="absolute" mt={-6} fontSize={14} fontWeight="bold" color="red.700" >
+                                <Text position="absolute" mt={-6} fontSize={14} fontWeight="bold" color="red.700">Ce mot de passe n&apos;est pas autorisé</Text>
+                                    <Tooltip label={arrayOfErrors.map((error, index) => (<Text key={index} fontSize={12}>{error}</Text>))}>
+                                        <IconButton icon={<InfoIcon height={22}/>} aria-label="seeMore" h={4} variant="none" bgColor="transparent" boxShadow="none" />
+                                    </Tooltip>
+                                </Text> 
+                                }
+                            <Input name="password" id="password" isRequired type='password' placeholder='Mot de passe' borderRadius={15} borderColor={error === 1 || error === 4 ? "red.700" : "green.600"}/>
+                        </InputGroup>
+                        <InputGroup size='md' mt={6} zIndex={0}>
+                            <Input name="passwordConfirmation" zIndex={0} id="passwordConfirmation" isRequired type={'password'} placeholder='Confirmer le mot de passe' borderRadius={15} borderColor={error === 1 || error === 4 ? "red.700" : "green.600"}/>
+                        </InputGroup>
+                        <Center>
+                            <Button variant="goldenButton" type="submit" mt={8}>
+                                Modifier
+                            </Button>
+                        </Center>
+                    </FormControl>
                 </form>
             </Box>
         </Center>
