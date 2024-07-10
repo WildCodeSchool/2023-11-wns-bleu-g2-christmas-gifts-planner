fa0d3953df3aa80df06c8e8270d7baa17f45e7ab
Alexandre <richert.alexandre31@gmail.com>
2024-07-10 11:05:13 +0200

    resolver by id

--- backend/src/resolvers/ChannelResolver.ts
+++ backend/src/resolvers/ChannelResolver.ts
@@ -1,10 +1,19 @@
-import { Query } from "type-graphql";
+import { Query, Resolver, Arg } from "type-graphql";
 import Channel from "../entities/Channel";
 
+@Resolver(Channel)
 export default class ChannelResolver {
-  @Query(() => [Channel])
+  @Query(() => [Channel]) 
   async channels() {
     return Channel.find({
+      relations: { group_id: true },
+    });
+  } 
+
+  @Query(() => Channel, { nullable: true })
+  async channel(@Arg("id") id: number) {
+    return Channel.findOne({
+      where: { id }, 
       relations: { group_id: true },
     });
   }
