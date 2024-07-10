1f20b477727a2f7f2eae6848b7d050c8429fb30e
Robin <robin.kolasinski@gmail.com>
2024-07-10 12:49:25 +0200

    added schema for updateUser

--- frontend/src/graphql/generated/schema.ts
+++ frontend/src/graphql/generated/schema.ts
@@ -50,6 +50,12 @@ export type NewUserInputType = {
   lastName: Scalars['String'];
   password: Scalars['String'];
 };
+export type UpdateUserInputType = {
+  email: Scalars['String'];
+  firstName: Scalars['String'];
+  lastName: Scalars['String'];
+  password: Scalars['String'];
+};
 
 export type Query = {
   __typename?: 'Query';
@@ -277,3 +283,31 @@ export function useUsersLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<User
 export type UsersQueryHookResult = ReturnType<typeof useUsersQuery>;
 export type UsersLazyQueryHookResult = ReturnType<typeof useUsersLazyQuery>;
 export type UsersQueryResult = Apollo.QueryResult<UsersQuery, UsersQueryVariables>;
+
+
+export const UpdateUserDocument = gql`
+  mutation UpdateUser($data: UpdateUserInputType!) {
+    updateUser(data: $data) {
+      id
+      firstName
+      lastName
+      email
+      password
+      }
+  }
+`;
+export type UpdateUserMutationVariables = Exact<{
+  data: UpdateUserInputType;
+}>;
+
+export type UpdateUserMutation = { __typename?: 'Mutation', updateUser: { __typename?: 'User', id: string, firstName: string, lastName: string, email: string } };
+export type UpdateUserMutationFn = Apollo.MutationFunction<UpdateUserMutation, UpdateUserMutationVariables>;
+
+export function useUpdateUserMutation(baseOptions?: Apollo.MutationHookOptions<UpdateUserMutation, UpdateUserMutationVariables>) {
+    const options = {...defaultOptions, ...baseOptions};
+    return Apollo.useMutation<UpdateUserMutation, UpdateUserMutationVariables>(UpdateUserDocument, options);
+}
+
+export type UpdateUserMutationHookResult = ReturnType<typeof useUpdateUserMutation>;
+export type UpdateUserMutationResult = Apollo.MutationResult<UpdateUserMutation>;
+export type UpdateUserMutationOptions = Apollo.BaseMutationOptions<UpdateUserMutation, UpdateUserMutationVariables>;
