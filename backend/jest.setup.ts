343864d6a4216f5dd034ae28530968b75d2bdef0
Jasmine Grozinger <114686310+Jasminegrz@users.noreply.github.com>
2024-08-27 15:32:49 +0200

    [FIX] uptated jest config

--- backend/jest.setup.ts
+++ backend/jest.setup.ts
@@ -3,8 +3,11 @@ import { ASTNode, graphql, GraphQLSchema, print } from "graphql";
 import db, { clearDB } from "./src/config/db";
 import getSchema from "./src/config/schema";
 import { Maybe } from "type-graphql";
+import nodemailer from "nodemailer";
+import env from "../backend/src/env";
 
 export let schema: GraphQLSchema;
+let transporter: nodemailer.Transporter;
 
 export async function execute(
   operation: ASTNode,
@@ -24,6 +27,15 @@ export async function execute(
 beforeAll(async () => {
   await db.initialize();
   schema = await getSchema;
+
+  transporter = nodemailer.createTransport({
+    host: env.SMTP_HOST,
+    port: env.SMTP_PORT,
+    auth: {
+      user: env.SMTP_USER,
+      pass: env.SMTP_PASSWORD,
+    },
+  });
 });
 
 beforeEach(async () => {
@@ -31,5 +43,12 @@ beforeEach(async () => {
 });
 
 afterAll(async () => {
+  try {
+    if (transporter) {
+      transporter.close();
+    }
+  } catch (error) {
+    console.error("Error closing transporter: ", error);
+  }
   await db.destroy();
 });
