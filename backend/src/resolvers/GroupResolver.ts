670fb8af5040dbea91fff28ac80aaa6db0da8b01
Alexandre <richert.alexandre31@gmail.com>
2024-08-28 09:24:07 +0200

    channel created

--- backend/src/resolvers/GroupResolver.ts
+++ backend/src/resolvers/GroupResolver.ts
@@ -4,6 +4,8 @@ import { NewGroupInputType } from "../types/NewGroupInputType";
 import { ContextType } from "../types/ContextType";
 import { GraphQLError } from "graphql";
 import { findUserByEmail } from "../services/userService";
+import Channel from "../entities/Channel";
+import User from "../entities/User";
 
 /**
  * Resolver class for handling group-related operations.
@@ -74,12 +76,22 @@ export default class GroupResolver {
       const members = [];
       for (const email of data.members) {
         const user = await findUserByEmail(email);
-        if (!user) {
-          throw new GraphQLError(`User with email ${email} not found`);
-        }
+        // if (!user) {
+        //   throw new GraphQLError(`User with email ${email} not found`);
+        // }
         members.push(user);
+        newGroup.members = members.filter(member => member !== null) as User[]; // Add the members to the group
+        const channel = new Channel();
+      Object.assign(channel, {
+        name: `${user?.lastName} ${user?.firstName}'s channel`,
+        group: newGroup,
+      });
+      console.log(channel);
+      
+      await channel.save();
       }
-      newGroup.members = members; // Add the members to the group
+  
+  
     }
 
     // Save the new group to the database and return it
--- backend/src/resolvers/MemberResolver.ts
+++ backend/src/resolvers/MemberResolver.ts
@@ -1,13 +1,59 @@
-import { Arg, Authorized, Ctx, Mutation } from "type-graphql";
+import { Arg, Authorized, Ctx, Mutation, Query } from "type-graphql";
 import Group from "../entities/Group";
 import { ContextType } from "../types/ContextType";
 import { GraphQLError } from "graphql";
 import User from "../entities/User";
+import Channel from "../entities/Channel";
 
 /**
  * Resolver class for handling member-related operations.
  */
 export default class MemberResolver {
+
+  @Authorized()
+  @Query(() => [User])
+  async getMembersByGroupId(
+    @Arg("groupId") groupId: number,
+    @Ctx() ctx: ContextType
+  ) {
+    // Check if the current user is logged in
+    if (!ctx.currentUser) {
+      throw new GraphQLError("You need to be logged in");
+    }
+
+    // Find the group with the given ID and load its members
+    const group = await Group.findOne({
+      where: { id: groupId },
+      relations: { members: true },
+    });
+
+    // Throw an error if the group is not found
+    if (!group) {
+      throw new GraphQLError("Group not found");
+    }
+
+    // Iterate over the members and create a channel for each member
+    for (const member of group.members) {
+      // Check if a channel already exists for this member in this group
+      const existingChannel = await Channel.findOne({
+        where: { name: `${member.lastName} ${member.firstName}'s channel`, group: { id: groupId } },
+      });
+
+      // If the channel doesn't exist, create a new one
+      if (!existingChannel) {
+        const channel = new Channel();
+        Object.assign(channel, {
+          name: `${member.lastName} ${member.firstName}'s channel`,
+          group: group,
+        });
+        await channel.save();
+      }
+    }
+
+    // Return the list of members
+    return group.members;
+  }
+
   /**
    * Adds a member to a group.
    */
@@ -49,9 +95,20 @@ export default class MemberResolver {
 
     // Add the user to the group's members list and save the changes to the database
     group.members.push(user);
+    
     await group.save();
 
+    const channel = new Channel();
+    Object.assign(channel, {
+      name: `${user.lastName} ${user.firstName}'s channel`,
+      group: group,
+    });
+    console.log(channel);
+    
+    await channel.save();
+
     // Return the updated group
+    
     return group;
   }
 }
