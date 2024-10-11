3f7dd221c5fbaa50775804d6e0aa3eb749e36954
Jasmine Grozinger <114686310+Jasminegrz@users.noreply.github.com>
2024-10-11 09:41:55 +0200

    hange path i18n file

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -1,7 +1,7 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import enTranslation from "./src/locales/en/translation.json";
-import frTranslation from "./src/locales/fr/translation.json";
+import enTranslation from "./locales/en/translation.json";
+import frTranslation from "./locales/fr/translation.json";
 // the translations
 // (tip move them in a JSON file and import them,
 // or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
