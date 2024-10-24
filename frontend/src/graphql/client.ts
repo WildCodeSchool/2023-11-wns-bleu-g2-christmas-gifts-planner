0f07ef51ea104664a6eb5cb846c78d17fb9b7654
mistral-nw <114388870+mistral-nw@users.noreply.github.com>
2024-10-24 14:17:23 +0200

    Update client.ts wsLink var

--- frontend/src/graphql/client.ts
+++ frontend/src/graphql/client.ts
@@ -13,7 +13,7 @@ const httpLink = new HttpLink({
 
 const wsLink = new GraphQLWsLink(
   createClient({
-    url: "ws://localhost:4001/subscriptions",
+    url: "ws://1123-bleu-2.wns.wilders.dev/subscriptions",
   })
 );
 
