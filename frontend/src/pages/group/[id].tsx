0646dd30b8cea2e6c3330a30eb2313485b4e388a
Alexandre <richert.alexandre31@gmail.com>
2024-10-26 17:34:10 +0200

    conflict managing

--- frontend/src/pages/group/[id].tsx
+++ frontend/src/pages/group/[id].tsx
@@ -28,11 +28,8 @@ import { useTranslation } from "react-i18next";
 import { useGroupContext } from "@/contexts/GroupContext";
 import { useFormValidation } from "@/hooks/useFormValidation";
 import { ApolloError } from "@apollo/client";
-<<<<<<< HEAD
 import SearchBar from "@/components/SearchBar";
-=======
 import { useAuthRedirect } from "@/hooks/useAuthRedirect";
->>>>>>> 5209f30e982c80cb73f5967ee9ba7a9cf3b2dc33
 
 export default function Channels() {
   const router = useRouter();
