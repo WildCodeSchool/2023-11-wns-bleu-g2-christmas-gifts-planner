3f5ae5c0f0a5c53b07d8ac9e67178355fc9a632a
Jasmine Grozinger <114686310+Jasminegrz@users.noreply.github.com>
2024-11-03 12:31:44 +0100

    delete the unused request and remove excess spaces

--- backend/src/resolvers/MemberResolver.ts
+++ backend/src/resolvers/MemberResolver.ts
@@ -11,49 +11,6 @@ import User from "../entities/User";
  * Resolver class for handling member-related operations.
  */
 export default class MemberResolver {
-
-  @Authorized()
-  @Query(() => [User])
-  async getMembersByGroupId(
-    @Arg("groupId") groupId: number,
-    @Ctx() ctx: ContextType
-  ) {
-    // Check if the current user is logged in
-    if (!ctx.currentUser) {
-      throw new GraphQLError("You need to be logged in");
-    }
-
-    // Find the group with the given ID and load its members
-    const group = await Group.findOne({
-      where: { id: groupId },
-      relations: { members: true },
-    });
-
-    // Throw an error if the group is not found
-    if (!group) {
-      throw new GraphQLError("Group not found");
-    }
-
-    for (const member of group.members) {
-      const existingChannel = await Channel.findOne({
-        where: { name: `${member.lastName} ${member.firstName}'s channel`, group: { id: groupId } },
-      });
-
-      if (!existingChannel) {
-        const channel = new Channel();
-        Object.assign(channel, {
-          name: `${member.lastName} ${member.firstName}'s channel`,
-          group: group,
-        });
-        
-        await channel.save();
-      }
-    }
-
-    // Return the list of members
-    return group.members;
-  }
-
   /**
    * Adds a member to a group.
    */
@@ -64,7 +21,6 @@ export default class MemberResolver {
     @Arg("data", { validate: true }) data: AddMembersInputType,
     @Ctx() ctx: ContextType
   ) {
-   
     if (!ctx.currentUser) {
       throw new GraphQLError("You need to be logged in");
     }
@@ -74,43 +30,29 @@ export default class MemberResolver {
       throw new GraphQLError("No members provided");
     }
 
-    // Verify if data contains members
-    if (!data.members || data.members.length === 0) {
-      throw new GraphQLError("No members provided");
-    }
-
-    
     const groupToUpdate = await Group.findOne({
       where: { id },
     });
 
-    
     if (!groupToUpdate) {
       throw new GraphQLError("Group not found");
     }
 
-    
     if (groupToUpdate.owner.id !== ctx.currentUser.id) {
       throw new GraphQLError("You are not the owner of this group");
     }
 
-  
-
     const existingMembersErrors: string[] = [];
-
-    
     if (data.members && data.members.length > 0) {
       for (const memberInput of data.members) {
         const user = await findOrCreateUserByEmail(memberInput.email);
-       
+
         if (groupToUpdate.members.some((member) => member.id === user.id)) {
           existingMembersErrors.push(
             `User with email ${memberInput.email} is already a member`
           );
         }
-        
         sendAnEmail(groupToUpdate, user, id);
-        
         groupToUpdate.members.push(user);
       }
     }
@@ -120,7 +62,6 @@ export default class MemberResolver {
 
     await groupToUpdate.save();
 
-    
     return Group.findOne({
       where: { id },
     });
